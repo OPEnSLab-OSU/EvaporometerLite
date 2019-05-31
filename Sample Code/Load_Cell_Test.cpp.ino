173bbea8931165f6d24d646851ca2c9e8769dcd9
fickmoleymath <45349071+fickmoleymath@users.noreply.github.com>
2019-05-31 14:48:18 -0700

    Loadcell Testing Upload

---
+++ Sample Code/Load_Cell_Test.cpp.ino
@@ -0,0 +1,17 @@
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+
+Adafruit_ADS1115 ads(0x48);
+
+void setup(){
+  Serial.begin(9600);
+  ads.begin();
+  ads.setGain(GAIN_ONE);
+}
+
+void loop(){
+  int16_t val;
+  val=ads.readADC_SingleEnded(0);
+  Serial.println(val);
+  delay(1000);
+}
