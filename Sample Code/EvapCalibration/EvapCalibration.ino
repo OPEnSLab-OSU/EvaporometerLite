2010251a3d5bb26cd84d95ef85334b77ce431182
elijahshumway <48225440+elijahshumway@users.noreply.github.com>
2019-10-07 15:45:37 -0700

    Add files via upload

--- Sample Code/EvapCalibration/EvapCalibration.ino
+++ Sample Code/EvapCalibration/EvapCalibration.ino
@@ -1,6 +1,12 @@
 #include <Wire.h>
 #include <Adafruit_ADS1015.h>
 
+#define CALIBRATE 1 //define as 1 to run in calibration mode
+#define CHECK 0     //define as 1 to run in check mode to verify calibration
+
+#define slope 0     //set to calculated slope from linear regression
+#define zero 0      //set to calculated zero from linear regression
+
 Adafruit_ADS1115 ads1115(0x48);
 
 int samples = 20; //set how many samples are taken for averaging
@@ -11,11 +17,28 @@ void setup() {
 }
 
 void loop() {
-  int val = 0; //reset val to 0 after each average
-  for(int i=0; i<samples; i++){
-     val+=ads1115.readADC_SingleEnded(0); //reads value from ADC
-  }
-  val = val/samples;  //averages value 
-  Serial.println(val); //prints value to terminal
-  delay(250);
+  #ifdef CALIBRATE
+    int val = 0; //reset val to 0 after each average
+    for(int i=0; i<samples; i++){
+       val+=ads1115.readADC_SingleEnded(0); //reads value from ADC
+    }
+    val = val/samples;  //averages measured value 
+    Serial.println(val); //prints value to terminal
+    delay(250);
+  #endif
+
+  #ifdef CHECK
+    int digital_weight = 0;
+    int check_val = 0;
+    for(int i=0; i<samples; i++) 
+      check_val+=ads1115.readADC_SingleEnded(0); //reads value from ADC
+    
+    check_val = check_val/samples; //averages measured value
+    
+    digital_weight*=slope;
+    digital_weight-=zero;
+    
+    Serial.println(digital_weight);
+    delay(250);
+  #endif
 }
